de614ef9c7287f0212a3781f881fce3fbd86666c
arnobchowdhury-coder <arnobchy2006@gmail.com>
2025-10-07 20:59:07 +0600

    added footer section

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router";
 import './index.css'
 import App from './App.jsx'
 import NavBar from './Components/NavBar/NavBar.jsx';
+import Footer from './Components/Footer/Footer.jsx';
 
 
 
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <NavBar></NavBar>
+      <Footer></Footer>
     </BrowserRouter>,
   </StrictMode>,
 )
