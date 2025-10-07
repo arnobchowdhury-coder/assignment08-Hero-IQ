4aae11d21dfd4d9bbb503dd00634cd4e8b740275
arnobchowdhury-coder <arnobchy2006@gmail.com>
2025-10-07 21:01:22 +0600

    keep some changes

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="footer footer-horizontal footer-center bg-base-200 text-base-content rounded p-10 w-11/12 mx-auto bg-gray-300 font-semibold">
+    <footer className="footer footer-horizontal footer-center bg-base-200 text-base-content rounded p-10 w-full mx-auto bg-gray-300 font-semibold">
       <nav className="grid grid-flow-col gap-4">
         <a className="link link-hover">About us</a>
         <a className="link link-hover">Contact</a>
