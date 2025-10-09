0d1be044ebb9bd0f2e30a6a4aa7bd82f92750dad
arnobchowdhury-coder <arnobchy2006@gmail.com>
2025-10-09 19:00:01 +0600

    added some change

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Hero from '../Hero/Hero';
+import TrendingApps from '../TrendingApps/TrendingApps';
+import { useLoaderData } from 'react-router';
 
 
 const Home = () => {
+  const apps = useLoaderData().slice(0, 8);
   return (
     <div>
       <Hero></Hero>
+      <TrendingApps apps={apps}></TrendingApps>
     </div>
   );
 };
